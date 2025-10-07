a0c290806ebd08b5ab799ea0f8fb617f29d06503
Baiju Thomas <baijubaby4880@gmail.com>
2025-10-07 21:14:40 +0530

    Adding new files

--- oops-concepts/encap-abstract/script.js
+++ oops-concepts/encap-abstract/script.js
@@ -1,11 +1,28 @@
-const user = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30,
+// const user = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 31,
+//     getUserInfo : function () {
+//     return new Date().getFullYear() - user.age;
+// }
+
+// }
+
+function createUser(firstName, lastName, age) {
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+        getUserInfo: function () {
+            return new Date().getFullYear() - this.age;
+        }
+    };
 }
 
-function ageOfPerson(params) {
-    return new Date().getFullYear() - params;
-}
-console.log(ageOfPerson(user.age));
 
+    const user1 = createUser("John", "Doe", 31); 
+    console.log(user1);
+    console.log(user1.getUserInfo());
+    const user2 = createUser("black", "yoo", 22); 
+    console.log(user2);
+    console.log(user2.getUserInfo());
