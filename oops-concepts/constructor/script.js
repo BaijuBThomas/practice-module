8ff7c338416c33a443c42ae643d48e32cf2ad594
Baiju Thomas <baijubaby4880@gmail.com>
2025-10-08 23:25:31 +0530

    updating a file

--- oops-concepts/constructor/script.js
+++ oops-concepts/constructor/script.js
@@ -3,16 +3,27 @@ function createUser(firstName, lastName, age) {
     firstName,
     lastName,
     age,
-    getAgeYear () {
-      return new Date().getFullYear() - user.age
+    getAgeYear() {
+      return new Date().getFullYear() - user.age;
     },
-  }
-  return user
+  };
+  return user;
 }
 
+const user1 = createUser('Aman', 'Mishra', 32);
+const user2 = createUser('Anurag', 'Singh', 72);
 
-const user1 = createUser('Aman', 'Mishra', 32)
-const user2 = createUser('Anurag', 'Singh', 72)
+const arr1 = [1, 2];
+const arr2 = [3, 4];
 
-const arr1 = [1,2]
-const arr2 = [3, 4]
+// 🆕 Added function to merge arrays and log user info
+function showUserDetailsAndMergeArrays(u1, u2, a1, a2) {
+  console.log(`${u1.firstName} ${u1.lastName} was born in ${u1.getAgeYear()}`);
+  console.log(`${u2.firstName} ${u2.lastName} was born in ${u2.getAgeYear()}`);
+  
+  const mergedArray = [...a1, ...a2];
+  console.log('Merged Array:', mergedArray);
+  return mergedArray;
+}
+
+showUserDetailsAndMergeArrays(user1, user2, arr1, arr2);
